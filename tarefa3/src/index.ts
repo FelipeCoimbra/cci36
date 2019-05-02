c0d8e281a8652b2f5664f4dca6bf5646cdc9c274
Luis Holanda <luiscmholanda@gmail.com>
2019-05-02 11:48:58 -0300

    Add BattleShip{Scene,Sensor} implementation

--- tarefa3/src/index.ts
+++ tarefa3/src/index.ts
@@ -1,10 +1,11 @@
 import THREE from "three";
+import "./controls.js";
 
 //
 // Instantiate THREE js renderer
 //
 
-let renderer = new THREE.WebGLRenderer({ antialias: true });
+let renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x220000, 1);
@@ -14,57 +15,411 @@ document.body.appendChild(renderer.domElement);
 const BOARD_SIZE = 10 as const;
 const SHIP_COUNT = [1, 3, 1] as const;
 
+const lastItem = <T>(xs: { 0: T } & T[]): T  => xs[xs.length - 1];
 
-/**
- *  Game scene manager. Manages game visual aspects (geometries, colors) and provides a high level API
+/*!
+ *  Game scene manager. Manages game visual aspects (geometries, colors) from a high level API
 */
 class BattleShipScene {
+    private static P1BarrierGridPosition = new THREE.Vector3(-2, 1.01, 0);
+    private static P1GridPosition = new THREE.Vector3(-2, 1.25, -0.24);
 
-  constructor(scene:THREE.Scene, battle:BattleShipGame) {
+    private shipsGroup: THREE.Group;
+    private pinsGroup: THREE.Group;
 
-  }
+    public player1 = new THREE.Group();
+    public player2 = new THREE.Group();
 
-  public abstract changePlayer();
+    get ships(): { 0: THREE.Object3D } & THREE.Object3D[] {
+      return this.shipsGroup.children as any;
+    }
 
-  public abstract selectShip(idx: number);
-  public abstract moveShip(idx: number, to: [number, number]);
-  public abstract settleShip(idx: number);
-  public abstract rotateShip(idx: number);
+    get pins(): { 0: THREE.Object3D } & THREE.Object3D[] {
+      return this.pinsGroup.children as any;
+    }
 
-  public abstract selectPin();
-  public abstract movePin(to: [number, number]);
-  public abstract settlePin();
+    private firstPlayer: boolean = true;
 
+    constructor(private scene: THREE.Scene) {
+        this.makeBoard();
+        this.shipsGroup = new THREE.Group();
+        this.pinsGroup = new THREE.Group();
+
+        this.scene.add(this.shipsGroup);
+        this.scene.add(this.pinsGroup);
+
+        this.makeShip(2);
+    };
+
+    public changePlayer(): void {
+        this.firstPlayer = !this.firstPlayer;
+
+        const rotation = new THREE.Matrix4();
+        rotation.makeRotationZ(Math.PI / 60);
+
+        this.animate(() => {
+            rotation.multiplyVector3(camera.position);
+            camera.lookAt(new THREE.Vector3(0, 0, 0));
+        });
+    }
+
+    public selectShip(): void {
+        const ship = lastItem(this.ships);
+
+        const delta = 2 / 60;
+        this.animate(() => ship.position.z += delta);
+    }
+
+    public settleShip(): void {
+        const ship = lastItem(this.ships);
+
+        const delta = 2 / 60;
+        this.animate(() => ship.position.z -= delta);
+    }
+
+    public moveShip(to: [number, number]): void {
+        const ship = lastItem(this.ships);
+        const finalPos = this.gridPosition(to);
+
+        let deltaPos = new THREE.Vector3();
+        deltaPos.subVectors(finalPos, ship.position);
+        deltaPos.divideScalar(60);
+
+        this.animate(() => ship.position.add(deltaPos));
+    }
+
+    public rotateShip(): void {
+        const ship = lastItem(this.ships);
+
+        this.animate(() => ship.rotateZ(Math.PI / 120));
+    }
+
+    public selectPin(): void {
+        const pin = lastItem(this.pins);
+
+        const delta = 2 / 60;
+        this.animate(() => pin.position.z += delta);
+    }
+
+    public movePin(to: [number, number]): void {
+        const pin = lastItem(this.pins);
+        const finalPos = this.barrierGridPosition(to);
+
+        let deltaPos = new THREE.Vector3();
+        deltaPos.subVectors(finalPos, pin.position);
+        deltaPos.divideScalar(60);
+
+        this.animate(() => pin.position.add(deltaPos));
+    }
+
+    public settlePin(): void {
+        const pin = lastItem(this.pins);
+
+        const delta = 2 / 60;
+        this.animate(() => pin.position.z -= delta);
+    }
+
+    public makeShip(length: 2 | 3 | 4): void {
+        const ship = new THREE.Mesh(
+          new THREE.BoxBufferGeometry(0.5, 0.8 * length, 0.5),
+          new THREE.MeshBasicMaterial({
+            color: 0xEEEEEE,
+            side: THREE.DoubleSide
+          }));
+        ship.position.x = 7.5;
+        ship.position.y = -9;
+        ship.position.z = -0.24;
+
+        this.shipsGroup.add(ship);
+    }
+
+    public makePin(): void {
+        const pin = new THREE.Mesh(
+          new THREE.BoxBufferGeometry(0.5, 1, 0.5),
+          new THREE.MeshBasicMaterial({
+            color: 0xFF0000,
+            side: THREE.DoubleSide,
+          })
+        )
+
+        pin.position.x = 8.5;
+        pin.position.y = 8;
+        pin.position.z = -0.8;
+
+        this.pinsGroup.add(pin);
+    }
+
+    public locationInGrid(pos: THREE.Vector3): [number, number] | null {
+        let deltaPos = new THREE.Vector3();
+        deltaPos.subVectors(pos, BattleShipScene.P1GridPosition);
+        deltaPos.multiplyScalar(BOARD_SIZE / 9);
+
+        if (deltaPos.x >= 0 && deltaPos.y >= 9)
+            return [Math.floor(deltaPos.x), Math.floor(deltaPos.y)];
+        else
+            return null;
+    }
+
+    public locationInBarrierGrid(pos: THREE.Vector3): [number, number] | null {
+        let deltaPos = new THREE.Vector3();
+        deltaPos.subVectors(pos, BattleShipScene.P1BarrierGridPosition);
+        deltaPos.multiplyScalar(BOARD_SIZE / 9);
+
+        if (deltaPos.z >= 0 && deltaPos.x >= 9)
+            return [Math.floor(deltaPos.z), Math.floor(deltaPos.x)];
+        else
+            return null;
+    }
+
+    private makeBoard(): void {
+        const material1 = new THREE.MeshPhongMaterial({
+            color: 0x2121CE,
+            emissive: 0x1A3C8E,
+            specular: 0x7BC8F2,
+            shininess: 100,
+            fog: true,
+            side: THREE.DoubleSide,
+            vertexColors: THREE.NoColors,
+            flatShading: false,
+        });
+        const material2 = new THREE.MeshPhongMaterial({
+            color: 0x424242,
+            emissive: 0x1A3C8E,
+            specular: 0x7BC8F2,
+            shininess: 100,
+            fog: true,
+            side: THREE.DoubleSide,
+            vertexColors: THREE.NoColors,
+            flatShading: false,
+        });
+
+        this.player1 = this.makePlayerSide(material1);
+        this.player2 = this.makePlayerSide(material2);
+        this.player2.rotateZ(Math.PI);
+
+        this.scene.add(this.player1);
+        this.scene.add(this.player2);
+    }
+
+    private makePlayerSide(material: THREE.Material): THREE.Group {
+        let player = new THREE.Group();
+
+        const board = new THREE.Mesh(
+            new THREE.BoxBufferGeometry(12, 12, 1),
+            material,
+        );
+        board.position.z = -1;
+        board.position.y = -6;
+        player.add(board);
+
+        const playergrid = this.makeGrid();
+        playergrid.position.copy(BattleShipScene.P1GridPosition.clone());
+        player.add(playergrid);
+
+        const barrier = new THREE.Mesh(
+            new THREE.BoxBufferGeometry(12, 1, 12),
+            material
+        );
+        barrier.position.z = 5.5;
+        barrier.position.y = -0.5;
+        player.add(barrier);
+
+        const p1barrierGrid = this.makeGrid();
+        p1barrierGrid.rotateX(Math.PI / 2);
+        p1barrierGrid.position.copy(BattleShipScene.P1BarrierGridPosition.clone());
+        player.add(p1barrierGrid);
+
+        const p1box = new THREE.Mesh(
+          new THREE.BoxBufferGeometry(2, 2, 1),
+          material,
+        );
+        p1box.position.x = 7.5;
+        p1box.position.y = -9;
+        p1box.position.z = -1;
+        player.add(p1box);
+
+        return player;
+    }
+
+    private makeGrid(): THREE.Group {
+        let grid = new THREE.Group();
+        let size = 9 / BOARD_SIZE;
+        let white = new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            side: THREE.DoubleSide
+        });
+        let black = new THREE.MeshBasicMaterial({
+            color: 0x000000,
+            side: THREE.DoubleSide
+        });
+
+        for (let i = 0; i != BOARD_SIZE; i++) {
+            for (let j = 0; j != BOARD_SIZE; j++) {
+                let mesh = new THREE.Mesh(
+                    new THREE.PlaneBufferGeometry(size, size),
+                    ((i + j) & 1) === 0 ? black : white
+                );
+                mesh.position.copy(this.gridPosition([i, j]));
+
+                grid.add(mesh);
+            }
+        }
+
+        return grid;
+    }
+
+    private gridPosition(pos: [number, number]): THREE.Vector3 {
+        let gridPos = BattleShipScene.P1GridPosition.clone();
+        if (!this.firstPlayer) {
+          let matrix = new THREE.Matrix4();
+          matrix.makeRotationZ(Math.PI);
+          matrix.multiplyVector3(gridPos);
+        }
+
+        const size = 9 / BOARD_SIZE;
+
+        gridPos.x += pos[0] * size;
+        gridPos.y += pos[1] * size;
+
+        return gridPos;
+    }
+
+    private barrierGridPosition(pos: [number, number]): THREE.Vector3  {
+        let barGridPos = BattleShipScene.P1BarrierGridPosition.clone();
+        if (!this.firstPlayer) {
+          let matrix = new THREE.Matrix4();
+          matrix.makeRotationX(Math.PI / 2);
+          matrix.multiplyVector3(barGridPos);
+        }
+
+        const size = 9 / BOARD_SIZE;
+
+        barGridPos.z += pos[0] * size;
+        barGridPos.x += pos[1] * size;
+
+        return barGridPos;
+    }
+
+    private animate(animation: () => void): void {
+        let steps = 60;
+
+        const fullAnimation = () => {
+            steps--;
+            animation();
+
+            if (steps) setTimeout(fullAnimation, 16);
+        };
+
+        fullAnimation();
+    }
 }
 
 const enum BSEventKind {
-  /** An object was selected (a ship or a pin). */
-  SELECT,
-  /** An object was unselected (a ship or a pin). */
-  UNSELECT,
-  /** Mouse movent over the grid. */
-  MOVE,
-  /** Rotate a ship. */
-  ROTATE,
-};
+    /*! An object was selected (a ship or a pin). */
+    SELECT,
+    /*! An object was unselected (a ship or a pin). */
+    UNSELECT,
+    /*! Mouse movent over the grid. */
+    MOVE,
+    /*! Rotate a ship. */
+    ROTATE,
+}
+
+const enum BSMoveLoc {
+    SHIP_GRID,
+    PIN_GRID,
+}
 
 abstract class BattleShipEvent {
-  public abstract kind: BSEventKind;
+    public abstract kind: BSEventKind;
 }
+
+class BSSelectEvent extends BattleShipEvent {
+    public kind = BSEventKind.SELECT;
+}
+
+class BSUnselectEvent extends BattleShipEvent {
+    public kind = BSEventKind.UNSELECT;
+}
+
+class BSMoveEvent extends BattleShipEvent {
+    public kind = BSEventKind.MOVE;
+
+    constructor(public to: [number, number], public loc: BSMoveLoc) {
+      super();
+    }
+}
+
+class BSRotateEvent extends BattleShipEvent {
+    public kind = BSEventKind.ROTATE;
+}
+
 
 const enum ORIENTATION {
-  HORIZONTAL,
-  VERTICAL
+    HORIZONTAL,
+    VERTICAL
 }
 
-/**
+type Handler<E extends BattleShipEvent> = (event: E) => void;
+
+/*!
  *  Low level event handler. Translates low level interactions such as mouse up to high level
  *  game events.
 */
 class BattleShipSensor {
-  public sense(scene: BattleShipScene): BattleShipEvent | null {
-    return null;
-  }
+    public selectHandler?: Handler<BSSelectEvent> = undefined;
+    public unselectHandler?: Handler<BSUnselectEvent> = undefined;
+    public moveHandler?: Handler<BSMoveEvent> = undefined;
+    public rotateHandler?: Handler<BSRotateEvent> = undefined;
+
+    private raycaster = new THREE.Raycaster();
+    private selecting = false;
+
+    constructor(private scene: BattleShipScene) {
+      window.addEventListener('mousemove', eve => this.toMoveEvent(eve), false);
+      window.addEventListener('mouseup', () => this.selectEvent(), false);
+      window.addEventListener('mousedown', eve => this.unselectOrRotateEvent(eve), false);
+    }
+
+    private toMoveEvent(event: MouseEvent): void {
+      if (!this.moveHandler) return;
+
+      this.raycaster.setFromCamera({
+        x: (event.clientX / window.innerWidth) * 2 - 1,
+        y: - (event.clientY / window.innerHeight) * 2 + 1,
+      }, camera);
+
+      let intersects = this.raycaster.intersectObject(this.scene.player1);
+      if (!intersects) {
+          intersects = this.raycaster.intersectObject(this.scene.player2);
+          if (!intersects) return;
+      }
+
+      let pos = this.scene.locationInGrid(intersects[0].point);
+      if (pos) {
+        this.moveHandler(new BSMoveEvent(pos, BSMoveLoc.SHIP_GRID));
+      } else if (pos = this.scene.locationInBarrierGrid(intersects[0].point)) {
+        this.moveHandler(new BSMoveEvent(pos, BSMoveLoc.PIN_GRID));
+      }
+    }
+
+    private selectEvent(): void {
+        if (!this.selectHandler) return;
+
+        this.selecting = true;
+        this.selectHandler(new BSSelectEvent);
+    }
+
+    private unselectOrRotateEvent(event: MouseEvent): void {
+        if (this.rotateHandler && event.button === 2) {
+          return this.rotateHandler(new BSRotateEvent);
+        }
+
+        if (this.unselectHandler && this.selecting) {
+          this.selecting = false;
+          this.unselectHandler(new BSUnselectEvent);
+        }
+    }
 }
 
 /**
@@ -78,7 +433,7 @@ enum PLAYER {
 /** Board positions are pairs of integers (x,y). */
 type BoardPosition = [number, number];
 
-/** 
+/**
  * Ship piece class.
  * Holds information about a specific Ship object
  */
@@ -146,7 +501,7 @@ class ShipBoardCell extends BattleShipBoardCell {
  * Battleship board representation. This representations works both as a pin board
  * and as a ship board.
  * Initially filled with water, cells are modified to ship cells when a ship is settled
- * on them. 
+ * on them.
  */
 class BattleShipBoard {
   private board: BattleShipBoardCell[][];
@@ -157,7 +512,7 @@ class BattleShipBoard {
     this.boardSize = boardSize;
     this.initBoard();
   }
-  
+
   /**
    * Adds a ship piece to the board
    * @param ship Ship piece to add to the board
@@ -223,7 +578,7 @@ class BattleShipBoard {
       throw new Error(`Attack position (${x}, ${y}) already attacked`);
     }
     cell.attacked = true;
-    return cell.cellKind === BoardCellKind.SHIP ? [(cell as ShipBoardCell).shipId, (cell as ShipBoardCell).shipPart] 
+    return cell.cellKind === BoardCellKind.SHIP ? [(cell as ShipBoardCell).shipId, (cell as ShipBoardCell).shipPart]
     : null;
   }
 
@@ -296,7 +651,7 @@ class BattleShipPlayer {
   public attack(target: BoardPosition): [number, number] | null {
     return this.pinBoard.attack(target);
   }
-  
+
   /**
    * Grants damage in a specific ship and ship part
    * @param shipId Id of ship to be damaged
@@ -383,7 +738,7 @@ class BattleShipGame {
  */
 class BattleShipUpdater {
   private currentPlayer: PLAYER;
-  
+
   constructor(gameScene: BattleShipScene) {
     this.currentPlayer = PLAYER.P1;
   }
@@ -393,7 +748,7 @@ class BattleShipUpdater {
       return;
     }
 
-    
+
   }
 }
 
@@ -426,9 +781,9 @@ class BattleShip {
   constructor(scene:THREE.Scene) {
     this.gameSettings = new BattleShipSettings();
     this.gameState = new BattleShipGame(this.gameSettings);
-    this.gameScene = new BattleShipScene(scene, this.gameState);
+    this.gameScene = new BattleShipScene(scene);
     this.updater = new BattleShipUpdater(this.gameScene);
-    this.sensor = new BattleShipSensor();
+    this.sensor = new BattleShipSensor(this.gameScene);
     this.actor = new BattleShipActor();
   }
 
@@ -451,10 +806,9 @@ class BattleShip {
 
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-camera.position.x = 5;
-camera.position.z = 3;
-camera.position.y = -5;
-camera.up = new THREE.Vector3(0,0,1);
+camera.position.z = 12;
+camera.position.y = -16;
+camera.up = new THREE.Vector3(0, 0, 1);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 //
@@ -469,33 +823,36 @@ const game = new BattleShip(scene);
 //
 
 var lights = [];
-lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+lights[2] = new THREE.PointLight(0xffffff, 1, 0);
 
-lights[ 0 ].position.set( 0, 200, 0 );
-lights[ 1 ].position.set( 100, 200, 100 );
-lights[ 2 ].position.set( - 100, - 200, - 100 );
+lights[0].position.set(0, 20, 0);
+lights[1].position.set(10, 20, 10);
+lights[2].position.set(-10, -20, -10);
 
-scene.add( lights[ 0 ] );
-scene.add( lights[ 1 ] );
-scene.add( lights[ 2 ] );
+scene.add(lights[0]);
+scene.add(lights[1]);
+scene.add(lights[2]);
 
 //
 // Add ambient light to scene
 //
 
 let amblight = new THREE.AmbientLight(0x444444);
-scene.add(amblight)
+scene.add(amblight);
 
 //
 // Create orbit controls for observation and resize action
 //
 
+let controls = new THREE.OrbitControls(camera);
+controls.update();
+
 window.addEventListener("resize", function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }, false);
 
 //
@@ -503,10 +860,10 @@ window.addEventListener("resize", function () {
 //
 
 const run = () => {
-  requestAnimationFrame(run);
+    requestAnimationFrame(run);
 
-  game.update();
-  renderer.render(scene, camera);
-}
+    game.update();
+    renderer.render(scene, camera);
+};
 
 run();
